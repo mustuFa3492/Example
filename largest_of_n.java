4435f4507ca22ade45e3c69fc32eece416505580
mustuFa <gmansari0064@gmail.com>
2021-09-27 16:36:52 +0530

    first commit

---
+++ largest_of_n.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class largest_of_n {
+    public static void main(String[] args) {
+        int large,n;
+        int []a=new int[20];
+        System.out.println("Enter count of numbers you want to enter: ");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter number " + (i+1) + ": ");
+            a[i]=sc.nextInt();
+        }
+        // 5 3 7 2
+        large=a[0];
+        for (int i = 1; i < n; i++) {
+           if (large<=a[i]) 
+           {
+               large=a[i];
+           } 
+        }
+        System.out.println(large);
+    }
+}
